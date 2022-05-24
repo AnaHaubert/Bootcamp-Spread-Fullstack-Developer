529f5280ca9bfab8a9d2c79b80940d7c412bba14
Ana <anachaubertf@gmail.com>
2022-05-24 23:57:53 +0100

    Adicionando exercícios e certificado do curso Variáveis e tipos

---
+++ Variáveis e tipos/palindromo.js
@@ -0,0 +1,22 @@
+// solução 1
+function verificaPalindromo(string) {
+    if(!string) return 'string inexistente';
+
+    return string.split('').reverse().join('') === string;
+}
+
+// solução 2
+
+function verificaPalindromo2(string) {
+    if(!string) return 'string inexistente';
+
+    for(let i = 0; i < string.length / 2; i++) {
+        if(string[i] !== string[string.length - 1 - i]) {
+            return false;
+        }  
+    }
+
+    return true;
+}
+
+console.log(verificaPalindromo2('oiop'));
