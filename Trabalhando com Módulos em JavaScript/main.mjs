423b3ae414606c2ddcf7177578bfaba093927c6b
Ana <anachaubertf@gmail.com>
2022-05-27 18:20:58 +0100

    Incluindo exercício e certificado do curso Trabalhando com módulos em JavaScript

---
+++ Trabalhando com Módulos em JavaScript/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Ana', 26));
+console.log(mostraCidade('Ana', 'Porto'));
+console.log(mostraHobby('Ana','assistir séries'));
