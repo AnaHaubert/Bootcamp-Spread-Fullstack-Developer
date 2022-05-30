74e842e835dd687f43f4b5c6cf857b68bf7e4e8b
Ana <anachaubertf@gmail.com>
2022-05-30 16:45:50 +0100

    Adicionando exercício e certificado do curso Manipulando a DOM com JavaScript

--- JavaScript Assincrono/script.js
+++ JavaScript Assincrono/script.js
@@ -1,19 +1,21 @@
 const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
-const catBtn = document.getElementById('change-cat');
 
-const getCats = async() => {
-        const data = await fetch(BASE_URL)
-        .then(res => res.json())
-        .catch(e => console.log(e))
-        
-        return data.wepurl;
-}
+const getCats = async () => {
+	try {
+		const data = await fetch(BASE_URL);
+		const json = await data.json();
+		return json.webpurl;
+	} catch (e) {
+		console.log(e.message);
+	}
+};
 
 const loadImg = async () => {
-    const catImg = document.getElementById('cat');
-    catImg.src = await getCats();
-}
-
-catBtn.addEventListener('click', loadImg);
+	const img = document.getElementsByTagName('img')[0];
+	img.src = await getCats();
+};
 
 loadImg();
+
+const btn = document.getElementById('change-cat');
+btn.addEventListener('click', loadImg);
